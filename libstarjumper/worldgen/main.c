bcc0a4ad044553c490798c4cefa20e6cb6473cec
Don McCaughey <don@donm.cc>
2014-08-16 15:35:35 -0700

    Renamed "src" directory to "libstarjumper".

--- libstarjumper/worldgen/main.c
+++ libstarjumper/worldgen/main.c
@@ -1,15 +1,15 @@
-#import <getopt.h>
+#include <getopt.h>
 #include <libgen.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sf/sf.h>
-#include <traveller/traveller.h>
+#include <voyager/voyager.h>
 
 
 struct options
 {
   sf_string_t name;
-  trav_hex_coordinate_t hex_coordinate;
+  struct vgr_hex_coordinate hex_coordinate;
 };
 
 
@@ -27,26 +27,27 @@ static void
 initialize_options(struct options *options)
 {
   options->name = sf_string("No Name");
-  options->hex_coordinate = trav_hex_coordinate(1, 1);
+  options->hex_coordinate = (struct vgr_hex_coordinate) { .horizontal=1, .vertical=1, };
 }
 
 
 int
 main(int argc, char **argv)
 {
-  trav_init();
+  vgr_init();
   
   struct options options;
   initialize_options(&options);
   parse_options(argc, argv, &options);
   
   sf_random_t random = sf_random_from_random_state();
-  trav_world_t world = trav_world(options.name, options.hex_coordinate, random, &random);
+  vgr_world_t world = vgr_world(options.name, options.hex_coordinate, random, &random);
   
   sf_string_t description = sf_string_from(world);
   fprintf(stdout, "%s\n", sf_string_chars(description));
   
-  trav_fin();
+  vgr_fin();
+  vgr_memory_expect_alloc_count_zero();
   return EXIT_SUCCESS;
 }
 
@@ -92,14 +93,13 @@ parse_options(int argc, char **argv, struct options *options)
         options->name = sf_string(optarg);
         break;
       case 'x':
-        {
-          sf_string_t argument = sf_string(optarg);
-          options->hex_coordinate = trav_hex_coordinate_from_string(argument);
-          if ( ! options->hex_coordinate) {
-            fprintf(stderr, "ERROR: \"%s\" is not a valid hex coordinate\n", optarg);
-            print_usage_and_exit(argc, argv);
-          }
+      {
+        bool valid = vgr_hex_coordinate_from_string(optarg, &options->hex_coordinate);
+        if ( ! valid) {
+          fprintf(stderr, "ERROR: \"%s\" is not a valid hex coordinate\n", optarg);
+          print_usage_and_exit(argc, argv);
         }
+      }
         break;
       case '?':
         print_usage_and_exit(argc, argv);
